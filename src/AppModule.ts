34713b0e988e89495c93a8e38aee013a7b38fb92
Csaba Zamolyi <csaba.zamolyi@outlook.com>
2020-04-25 18:05:44 +0200

    Main module setup  - removed test basic ingredients module  - removed test views

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './AppController';
 import { AppService } from './AppService';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { BaseIngredient } from './Ingredients/BaseIngredients/BaseIngredient';
-import { BaseIngredientModule } from './Ingredients/BaseIngredients/BaseIngredientModule';
 
 // Main module
 @Module({
@@ -12,11 +10,13 @@ import { BaseIngredientModule } from './Ingredients/BaseIngredients/BaseIngredie
             type: 'sqlite',
             database: 'data/db.sqlite',
             synchronize: true,
-            entities: [
-                BaseIngredient,
-            ],
+            migrations: ['dist/migrations/*.js'],
+            cli: {
+                migrationsDir: 'migrations',
+            },
+            autoLoadEntities: true,
+            entities: [],
         }),
-        BaseIngredientModule,
     ],
     controllers: [AppController],
     providers: [AppService],
